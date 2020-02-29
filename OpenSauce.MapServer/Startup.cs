997774141de0f6efcb05bfa76b7509c4aa98a4a0
Martin Ballantyne <TheFieryScythe@gmail.com>
2020-02-29 16:37:41 +0000

    Use connection strings to get the application insights id

--- OpenSauce.MapServer/Startup.cs
+++ OpenSauce.MapServer/Startup.cs
@@ -23,7 +23,7 @@ namespace OpenSauce.MapServer
 				.AddXmlSerializerFormatters();
 
 			services.AddAzureClients(builder => { builder.AddBlobServiceClient(Configuration["ConnectionStrings:opensaucemapserverstorage"]); });
-			services.AddApplicationInsightsTelemetry(Configuration["ApplicationInsights:InstrumentationKey"]);
+			services.AddApplicationInsightsTelemetry(Configuration["ConnectionStrings:applicationinsights"]);
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
